3552f48f40fa7b22929a75e87069a2ddc6405719
Shannon Marsh <sjmarsh31@gmail.com>
2015-10-18 12:09:31 +1100

    Modify structure of nuspec to get nuget package working

--- MVC/GeneratorFedNuget/content/Settings.ps1.pp
+++ MVC/GeneratorFedNuget/content/Settings.ps1.pp
@@ -1,2 +1,2 @@
-$projectName = $rootnamespace$
+$projectName = "$rootnamespace$"
 #for more details on nuget source code transforms see: https://docs.nuget.org/create/configuration-file-and-source-code-transformations
---
+++ MVC/GeneratorFedNuget/readme.txt
@@ -0,0 +1,5 @@
+Front-end code generated in Content folder.  You may need to "Show-All" files/folders in visual studio to see it.
+
+More info:
+https://www.npmjs.com/package/generator-fed
+
