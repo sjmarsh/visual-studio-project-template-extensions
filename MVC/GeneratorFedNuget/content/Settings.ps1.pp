53630ea5961e73cf11f258e5da79372108bffcd8
Shannon Marsh <sjmarsh31@gmail.com>
2015-10-25 10:10:18 +1100

    Get GeneratorFed Nuspec to run in correct path

--- MVC/GeneratorFedNuget/content/Settings.ps1.pp
+++
@@ -1,2 +0,0 @@
-$projectName = "$rootnamespace$"
-#for more details on nuget source code transforms see: https://docs.nuget.org/create/configuration-file-and-source-code-transformations
--- MVC/GeneratorFedNuget/tools/Install.ps1
+++ MVC/GeneratorFedNuget/tools/Install.ps1
@@ -1,28 +1,31 @@
-. .\Settings.ps1
+param($installPath, $toolsPath, $package, $project)
+$projectFullName = $project.FullName
+$debugString = "install.ps1 executing for " + $projectFullName
+Write-Host $debugString
 
-if($projectName -eq $null){
-	$projectName = "TestProject" # for debug only!
-}
+$fileInfo = new-object -typename System.IO.FileInfo -ArgumentList $projectFullName
+$projectDirectory = $fileInfo.DirectoryName
 
-Write-Output "Checking for Content Folder..."
-if(Test-Path "\Content"){
-	Write-Output "Content Folder Exists"
+$projectName = $project.Name
+$contentPath = $projectName + "\Content"
+
+Write-Host "Checking for Content Folder..."
+if(Test-Path $contentPath){
+	Write-Host "Content Folder Exists"
 }
 else {
-	Write-Output "Creating Content Folder"
-	New-Item "Content" -type directory
+	Write-Host "Creating Content Folder"
+	New-Item "$contentPath" -type directory
 }
+cd $contentPath
 
-cd Content
-
-Write-Output "Installing generator-fed..."
+Write-Host "Installing generator-fed..."
 	& npm install yo generator-fed@^1 
 	.\node_modules\.bin\yo.cmd fed [--name $projectName] [--static] [--install]
 
-Write-Output "Installing node modules..."
+Write-Host "Installing node modules..."
 	& npm config set registry http://npm:8080 
 	& npm install --production --no-optional 
-Write-Output "Gulping..."
+Write-Host "Gulping..."
 	& node .\node_modules\gulp\bin\gulp all 
-
 cd ..
